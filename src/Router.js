3143eeea9ba9f2ffac60a8060cc0018064e7beac
Iván <127499949+ivanalame@users.noreply.github.com>
2024-01-04 16:49:00 +0100

    Revert "EmpresasCentro"

--- src/Router.js
+++ src/Router.js
@@ -4,8 +4,6 @@ import { BrowserRouter as Router, Routes, Route,useParams } from 'react-router-d
 import LayoutComponent from './components/LayoutComponent';
 import HomePage from './pages/HomePage';
 import LoginPage from './pages/LoginPage';
-import EmpresasCentro from './pages/EmpresasCentroPage';
-import CentroLayoutPage from './pages/CentroLayoutPage';
 import TodasCharlasPage from './pages/TodasCharlasPage';
 import InscripcionPage from './pages/InscripcionPage';
 import CharlasTechRiderPage from './pages/CharlasTechRiderPage';
@@ -16,7 +14,6 @@ const AppRouter = () => {
     return<CharlasTechRiderPage idtechrider = {idtechrider}/>
 }
   return (
-
 <Router>
   <LayoutComponent>
     <Routes>
